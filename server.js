4aba57f0227fdc9a20aa861354e9d0560953b051
Dharanikumar19 <dharanikumarmuthusamy@gmail.com>
2022-07-01 14:43:37 +0530

    second commit

--- server.js
+++ server.js
@@ -12,52 +12,31 @@ const datastore = new Datastore({
 });
 
 
-const insertVisit = visit => {
-  return datastore.save({
-    key: datastore.key('visit'),
-    data: visit,
-  });
-};
+async function quickstart() {
+  // The kind for the new entity
+  const kind = 'Task';
 
-const getVisits = () => {
-  const query = datastore
-    .createQuery('visit')
-    .order('timestamp', {descending: true})
-    .limit(10);
+  // The name/ID for the new entity
+  const name = 'sampletask1';
 
-  return datastore.runQuery(query);
-};
+  // The Cloud Datastore key for the new entity
+  const taskKey = datastore.key([kind, name]);
 
-app.get('/', async (req, res, next) => {
- 
-  const visit = {
-    timestamp: new Date(),
-   
-    userIp: crypto
-      .createHash('sha256')
-      .update(req.ip)
-      .digest('hex')
-      .substr(0, 7),
+  // Prepares the new entity
+  const task = {
+    key: taskKey,
+    data: {
+      description: 'Buy milk',
+    },
   };
 
-  try {
-    await insertVisit(visit);
-    const [entities] = await getVisits();
-    const visits = entities.map(
-      entity => `Time: ${entity.timestamp}, AddrHash: ${entity.userIp}`
-    );
-    res
-      .status(200)
-      .set('Content-Type', 'text/plain')
-      .send(`Last 10 visits:\n${visits.join('\n')}`)
-      .end();
-  } catch (error) {
-    next(error);
-  }
-});
+  // Saves the entity
+  await datastore.save(task);
+  console.log(`Saved ${task.key.name}: ${task.data.description}`);
+}
+quickstart();
 
 const PORT = parseInt(parseInt(process.env.PORT)) || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
 });
